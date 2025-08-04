437525339ac6027dd60ee7c6b36ca9bdeea6fff2
codewithfrancis <offor919@gmail.com>
2025-08-04 17:13:10 +0100

    push data to github

--- app/content/databases/page.tsx
+++ app/content/databases/page.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
-import { databases } from "@/data/db"
+import databases from "@/data/database"
 import Header from "@/components/shared/Header"
 import { ExternalLink, Database} from "lucide-react"
 
